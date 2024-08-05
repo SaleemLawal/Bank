89d6fefb07800f3a5a873286e2d84425f42c08e1
SaleemLawal <slawal1@umbc.edu>
2024-08-04 21:14:16 -0400

    added a route for transaction

--- Controllers/Controller.mjs
+++ Controllers/Controller.mjs
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import userRoutes from './UserController.mjs'
 import accountRoutes from './AccountsController.mjs'
+import transactionRoutes from './TransactionsController.mjs'
 
 const routes = Router()
 
 routes.use('/api', userRoutes)
 routes.use('/api', accountRoutes)
+routes.use('/api', transactionRoutes)
 
 export default routes
